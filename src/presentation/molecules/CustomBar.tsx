09f99e0208824098f846d868f15748bef976f8a0
sbateca <sergio.bateca@ioet.com>
2023-11-01 11:54:41 -0500

    feat: set report path in custom menu

--- src/presentation/molecules/CustomBar.tsx
+++ src/presentation/molecules/CustomBar.tsx
@@ -7,7 +7,7 @@ export const CustomBar = () => {
     return (<AppBar>
       <Stack spacing={0} sx={{ width: '100%' }}>
         <Typography component='p'>
-          Microlab MK S.A.S.
+          MKLab S.A.S.
         </Typography>
       </Stack>
     </AppBar>);
--- src/presentation/molecules/CustomMenu.tsx
+++ src/presentation/molecules/CustomMenu.tsx
@@ -1,12 +1,11 @@
 import { Menu } from "react-admin";
 import LabelIcon from '@mui/icons-material/Label';
-import BookIcon from '@mui/icons-material/Book';
 import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
-import PeopleIcon from '@mui/icons-material/People';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 import Collapse from '@mui/material/Collapse';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const collapseItemStyle = {
     paddingLeft: '20px'
@@ -26,7 +25,7 @@ export function CustomMenu() {
     return (
         <Menu>
             <Menu.DashboardItem />
-            <Menu.Item to="/billing" primaryText="Billing" leftIcon={<BookIcon />} />
+            <Menu.Item to="/reports" primaryText="Reporte de ensayos" leftIcon={<TextSnippetIcon />} />
             <Menu.Item
                 to="#"
                 primaryText="Billing"
